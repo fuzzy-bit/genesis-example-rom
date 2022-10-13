b951d9d3656f6abdf38f4e4cb6f490241f93c0a8
fuzzy <ezra@insicuri.net>
2022-10-12 21:45:33 -0400

    Attempt to fix indentation

--- src/Main.asm
+++ src/Main.asm
@@ -11,7 +11,7 @@ StartROM:
 		dc.l	$00000000, $00000000, $00000000, $00000000	; 
 		dc.l	$00000000, $00000000, $00000000, $00000000	; 
 		dc.l	$00000000, $00000000, $00000000, $00000000	; 
-		dc.l	$00000000, $00000000, VBlank,       $00000000	; screw it let's just do what rivet tells me to do 
+		dc.l	$00000000, $00000000, VBlank,	   $00000000	; screw it let's just do what rivet tells me to do 
 		dc.l	$00000000, $00000000, $00000000, $00000000	; 
 		dc.l	$00000000, $00000000, $00000000, $00000000	; 
 		dc.l	$00000000, $00000000, $00000000, $00000000	; 
@@ -38,19 +38,19 @@ StartROM:
 ; Start of code (Setup)
 ; ===========================================================================
 EntryPoint:
-		move.b	($A10001).l,d0				; load hardware version/region
-		andi.l	#$0F,d0						; get only the version number
-		beq.s	NoTMSS						; if the version is 0, branch (no TMSS in this machine)
+		move.b	($A10001).l,d0			; load hardware version/region
+		andi.l	#$0F,d0							; get only the version number
+		beq.s	NoTMSS							; if the version is 0, branch (no TMSS in this machine)
 		move.l	(ConsoleName).w,($A14000).l	; give TMSS the string "SEGA" so it unlocks the VDP
-		moveq	#$00,d0						; clear d0
+		moveq	#$00,d0							; clear d0
 
 NoTMSS:
 		move.w	#$0100,d1					; prepare Z80 value/VDP register increment
-		move.w	d1,($A11100).l				; request Z80 to stop
-		move.w	d0,($A11200).l				; request Z80 reset on (resets YM2612)
+		move.w	d1,($A11100).l			; request Z80 to stop
+		move.w	d0,($A11200).l			; request Z80 reset on (resets YM2612)
 		
 		lea	($C00000).l,a5					; load VDP data port
-		lea	$04(a5),a6						; load VDP control port
+		lea	$04(a5),a6							; load VDP control port
 		
 		move.w  #$8100|%01100100, VDPControl	; turn on screen, enable vblank, and set resolution
 		move.w  #$8000|%00000100, VDPControl	; disable low-color mode
@@ -67,17 +67,17 @@ NoTMSS:
 		move.w 	#$0E00,	VDPDataPort			; palette 0 slot 0
 		move.w 	#$0EE0,	VDPDataPort			; palette 0 slot 1
 MainLoop:
-		WaitVBL								; wait for vblank
-		vram	$0000						; set control port to beginning of VRAM
+		WaitVBL									; wait for vblank
+		vram	$0000							; set control port to beginning of VRAM
 		
-		lea 	TestTile, a0				; set a0 to point to tile data
+		lea 	TestTile, a0						; set a0 to point to tile data
 		move.w	#7, d0						; loop loading for 8 lines
 		bsr.w	LoadTiles					; load tiles into vram
 		
 		bra.w	MainLoop					; loop endlessly
 
 VBlank:
-		move.b	#1, (VBFlag)				; set vblank flag to true
+		move.b	#1, (VBFlag)			; set vblank flag to true
 		rte
 		
 		
